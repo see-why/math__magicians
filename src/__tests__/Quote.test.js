500ffb6111c0404c1519ae7a84eae3e99e63e36a
Tendai <tendai1@hotmail.com>
2021-08-25 22:14:31 +0200

    Add Qoute test

--- src/__tests__/Quote.test.js
+++ src/__tests__/Quote.test.js
@@ -1,4 +1,16 @@
 import React from 'react';
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import renderer from 'react-test-renderer';
 import Quote from '../components/Quote';
+
+describe('Testing the Qoute component', () => {
+  it('should render correctly to the dom', () => {
+    const quote = renderer.create(<Quote />).toJSON();
+    expect(quote).toMatchSnapshot();
+  });
+
+  it('should render without errors', () => {
+    render(<Quote />);
+  });
+});
