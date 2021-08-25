18e4db25497c3132c7dc32896f4fcdaf3379176f
Leandro <officialdevmi@gmail.com>
2021-08-25 06:06:50 -0700

    Fix inline styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,33 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
 
-const Navigation = () => {
-  const logo = {
-    flex: '2',
-    marginLeft: '10px',
-    fontSize: '2.8em',
-    fontFamily: "'Rampart One', cursive",
-    padding: '20px',
-  };
-
-  return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <span style={logo}>
-        <Link to="/math__magicians" className="Visited">Math Magicians</Link>
-      </span>
-      <ul className="link-container">
-        <li className="links">
-          <Link to="/math__magicians" className="Visited">Home</Link>
-        </li>
-        <li className="links">
-          <Link to="/math__magicians/calculator" className="Visited">Calculator</Link>
-        </li>
-        <li className="links">
-          <Link to="/math__magicians/quote" className="Visited">Quote</Link>
-        </li>
-      </ul>
-    </div>
-  );
-};
+const Navigation = () => (
+  <div className="nav-container">
+    <span className="logo">
+      <Link to="/math__magicians" className="Visited">Math Magicians</Link>
+    </span>
+    <ul className="link-container">
+      <li className="links">
+        <Link to="/math__magicians" className="Visited">Home</Link>
+      </li>
+      <li className="links">
+        <Link to="/math__magicians/calculator" className="Visited">Calculator</Link>
+      </li>
+      <li className="links">
+        <Link to="/math__magicians/quote" className="Visited">Quote</Link>
+      </li>
+    </ul>
+  </div>
+);
 
 export default Navigation;
