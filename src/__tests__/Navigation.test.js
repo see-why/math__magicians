a3b22eff7ff167231a8a9e268ad78cbdd28307c0
Tendai <tendai1@hotmail.com>
2021-08-25 22:41:00 +0200

    Add home test

--- src/__tests__/Navigation.test.js
+++ src/__tests__/Navigation.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {
-  render, fireEvent, waitFor, screen,
-} from '@testing-library/react';
+import {  render, fireEvent, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import renderer from 'react-test-renderer';
 import Navigation from '../components/Navigation';
+
